efebe4a5ba22b9041004218f7f2cc144a19dcaba
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2023-07-26 19:57:40 +0800

    update
    it can be load by KDU

--- Tools/Source.c
+++ Tools/Source.c
@@ -1,27 +1,24 @@
 ﻿#include <ntifs.h>
 #include <windef.h>
 #include <intrin.h>
-
+#include "global.h"
 #define printfs(x, ...) DbgPrintEx(0, 0, x, __VA_ARGS__)
 
-#define Mdl_Read CTL_CODE(FILE_DEVICE_UNKNOWN,0x800,METHOD_BUFFERED,FILE_ALL_ACCESS)
+NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName, PDRIVER_INITIALIZE InitializationFunction);
 
-#define Mdl_Write CTL_CODE(FILE_DEVICE_UNKNOWN,0x801,METHOD_BUFFERED,FILE_ALL_ACCESS)
 
-typedef struct _UserData
-{
-	DWORD Pid;	
-	DWORD64 Address;
-	DWORD Size;
-	PBYTE Data;	
-}UserData, * PUserData;
+#define init_code CTL_CODE(FILE_DEVICE_UNKNOWN, 0x775, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define read_code CTL_CODE(FILE_DEVICE_UNKNOWN, 0x776, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define write_code CTL_CODE(FILE_DEVICE_UNKNOWN, 0x777, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
 
-#define DIRECTORY_TABLE_BASE 0x028
-#pragma intrinsic(_disable)
-#pragma intrinsic(_enable)
-NTKERNELAPI NTSTATUS PsLookupProcessByProcessId(HANDLE ProcessId, PEPROCESS
-	* Process);
-NTKERNELAPI CHAR* PsGetProcessImageFileName(PEPROCESS Process);
+typedef struct info_t {
+	HANDLE target_pid;
+	void* target_address;
+	PBYTE buffer_address;
+	SIZE_T Size;
+	SIZE_T return_size;
+} UserData, * PUserData;
+
 KIRQL Open()
 {
 	KIRQL irql = KeRaiseIrqlToDpcLevel();
@@ -48,7 +45,7 @@ ULONG64 CheckAddressVal(PVOID p)
 
 ULONG64 Attach(IN PEPROCESS Process) {
 	ULONG64 pDTB = 0, OldCr3 = 0, vAddr = 0;
-	pDTB = CheckAddressVal((UCHAR*)Process + DIRECTORY_TABLE_BASE);
+	pDTB = CheckAddressVal((UCHAR*)Process + 0x28);
 	if (pDTB == 0)
 	{
 		return FALSE;
@@ -60,154 +57,151 @@ ULONG64 Attach(IN PEPROCESS Process) {
 	return OldCr3;
 }
 
-VOID Deattach(IN ULONG64 OldCr3) {
+VOID DeAttach(IN ULONG64 OldCr3) {
 	_disable();
 	__writecr3(OldCr3);
 	_enable();
 
 }
 
-UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\SFeather");
-UNICODE_STRING DeviceLink = RTL_CONSTANT_STRING(L"\\??\\SFeather");
+NTSTATUS ctl_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
 
-VOID MdlReadProcessMemory(PUserData Buffer)
-{
-	PEPROCESS Process = NULL;
-	NTSTATUS Status = PsLookupProcessByProcessId((HANDLE)Buffer->Pid, &Process);
-	if (!NT_SUCCESS(Status))
-	{
-		printfs("[Mdl] : read PsLookupProcessByProcessId FAIL");
-		return;
+	static PEPROCESS Process;
+
+	irp->IoStatus.Information = sizeof(UserData);
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(irp);
+	PUserData Buffer = (PUserData)irp->AssociatedIrp.SystemBuffer;
+	DbgPrint("IOCTL\n");
+
+	if (stack) { //add error checking
+		if (Buffer && sizeof(*Buffer) >= sizeof(UserData)) {
+			ULONG ctl_code = stack->Parameters.DeviceIoControl.IoControlCode;
+			if (ctl_code == init_code)
+				PsLookupProcessByProcessId(Buffer->target_pid, &Process);
+
+			else if (ctl_code == read_code) {
+				DbgPrint("READ\n");
+				PBYTE Temp = ExAllocatePool(PagedPool, Buffer->Size);
+				if (Temp == NULL)
+				{
+					printfs("[Mdl] : read ExAllocatePool FAIL");
+					ObDereferenceObject(Process);
+					return;
+				}
+
+				ULONG64 OldCr3 = Attach(Process);
+
+				ProbeForRead((PVOID)Buffer->target_address, Buffer->Size, 1);
+				RtlCopyMemory(Temp, (PVOID)Buffer->target_address, Buffer->Size);
+
+				DeAttach(OldCr3);
+
+				RtlCopyMemory(Buffer->buffer_address, Temp, Buffer->Size);
+				DbgPrint("%d\n", *Buffer->buffer_address);
+				ExFreePool(Temp);
+			}
+
+			else if (ctl_code == write_code) {
+				DbgPrint("WRITE\n");
+
+				PBYTE Temp = ExAllocatePool(PagedPool, Buffer->Size);
+				if (Temp == NULL)
+				{
+					printfs("[Mdl] : write ExAllocatePool FAIL");
+					ObDereferenceObject(Process);
+					return;
+				}
+
+				for (DWORD i = 0; i < Buffer->Size; i++) Temp[i] = Buffer->buffer_address[i];
+
+				ULONG64 OldCr3 = Attach(Process);
+
+				PMDL Mdl = IoAllocateMdl((PVOID)Buffer->target_address, Buffer->Size, FALSE, FALSE, NULL);
+				if (Mdl == NULL)
+				{
+					printfs("[Mdl] : IoAllocateMdl Fail");
+					ExFreePool(Temp);
+					ObDereferenceObject(Process);
+					return;
+				}
+
+				MmBuildMdlForNonPagedPool(Mdl);
+
+				PBYTE ChangeData = MmMapLockedPages(Mdl, KernelMode);
+
+				if (ChangeData) RtlCopyMemory(ChangeData, Temp, Buffer->Size);
+
+				IoFreeMdl(Mdl);
+				ExFreePool(Temp);
+				DeAttach(OldCr3);
+			}
+			irp->IoStatus.Information = sizeof(UserData);
+
+		}
 	}
 
-	PBYTE Temp = ExAllocatePool(PagedPool, Buffer->Size);
-	if (Temp == NULL)
-	{
-		printfs("[Mdl] : read ExAllocatePool FAIL");
-		ObDereferenceObject(Process);
-		return;
-	}
-
-	ULONG64 OldCr3 = Attach(Process);
-
-	ProbeForRead((PVOID)Buffer->Address, Buffer->Size, 1);
-
-	RtlCopyMemory(Temp, (PVOID)Buffer->Address, Buffer->Size);
-
-	ObDereferenceObject(Process);
-
-	Deattach(OldCr3);
-
-	RtlCopyMemory(Buffer->Data, Temp, Buffer->Size);
-
-	ExFreePool(Temp);
-}
-
-VOID MdlWriteProcessMemory(PUserData Buffer)
-{
-	PEPROCESS Process = NULL;
-	NTSTATUS Status = PsLookupProcessByProcessId((HANDLE)Buffer->Pid, &Process);
-	if (!NT_SUCCESS(Status))
-	{
-		printfs("[Mdl] : write PsLookupProcessByProcessId FAIL");
-		return;
-	}
-
-	PBYTE Temp = ExAllocatePool(PagedPool, Buffer->Size);
-	if (Temp == NULL)
-	{
-		printfs("[Mdl] : write ExAllocatePool FAIL");
-		ObDereferenceObject(Process);
-		return;
-	}
-
-	for (DWORD i = 0; i < Buffer->Size; i++) Temp[i] = Buffer->Data[i];
-
-	ULONG64 OldCr3 = Attach(Process);
-
-	PMDL Mdl = IoAllocateMdl((PVOID)Buffer->Address, Buffer->Size, FALSE, FALSE, NULL);
-	if (Mdl == NULL)
-	{
-		printfs("[Mdl] : IoAllocateMdl Fail");
-		ExFreePool(Temp);
-		ObDereferenceObject(Process);
-		return;
-	}
-
-	MmBuildMdlForNonPagedPool(Mdl);
-
-	PBYTE ChangeData = MmMapLockedPages(Mdl, KernelMode);
-
-	if (ChangeData) RtlCopyMemory(ChangeData, Temp, Buffer->Size);
-
-	IoFreeMdl(Mdl);
-	ExFreePool(Temp);
-	Deattach(OldCr3);
-	ObDereferenceObject(Process);
-}
-
-NTSTATUS DriverIoctl(PDEVICE_OBJECT Device, PIRP pirp)
-{
-	UNREFERENCED_PARAMETER(Device);
-
-	PIO_STACK_LOCATION Stack = IoGetCurrentIrpStackLocation(pirp);
-
-	ULONG Code = Stack->Parameters.DeviceIoControl.IoControlCode;
-
-	if (Stack->MajorFunction == IRP_MJ_DEVICE_CONTROL)
-	{
-		PUserData Buffer = pirp->AssociatedIrp.SystemBuffer;
-		printfs("[Mdl] : PID:%d  Addr ַ:%x  Size:%d", Buffer->Pid, Buffer->Address, Buffer->Size);
-
-		if (Code == Mdl_Read) MdlReadProcessMemory(Buffer);
-		if (Code == Mdl_Write) MdlWriteProcessMemory(Buffer);
-
-		pirp->IoStatus.Information = sizeof(UserData);
-	}
-	else pirp->IoStatus.Information = 0;
-
-	pirp->IoStatus.Status = STATUS_SUCCESS;
-	IoCompleteRequest(pirp, IO_NO_INCREMENT);
-
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
 	return STATUS_SUCCESS;
 }
 
-VOID DriverUnload(PDRIVER_OBJECT object)
-{
-	if (object->DeviceObject)
-	{
-		IoDeleteSymbolicLink(&DeviceLink);
-		IoDeleteDevice(object->DeviceObject);
-	}
-	printfs("[Mdl] : Unload");
+NTSTATUS unsupported_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
+
+	irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT object, PUNICODE_STRING reg)
-{
-	printfs("[Mdl] : LOAD -> %wZ", reg);
+NTSTATUS create_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
 
-	object->DriverUnload = DriverUnload;
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
+}
 
-	PDEVICE_OBJECT Device = NULL;
-	NTSTATUS Status = IoCreateDevice(object, sizeof(object->DriverExtension), &DeviceName, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &Device);
-	if (!NT_SUCCESS(Status))
-	{
-		printfs("[Mdl] : IoCreateDevice FAIL");
-		return Status;
-	}
+NTSTATUS close_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
 
-	Status = IoCreateSymbolicLink(&DeviceLink, &DeviceName);
-	if (!NT_SUCCESS(Status))
-	{
-		printfs("[Mdl] : IoCreateSymbolicLink FAIL");
-		IoDeleteDevice(Device);
-		return Status;
-	}
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
+}
 
-	object->MajorFunction[IRP_MJ_CREATE] = DriverIoctl;
-	object->MajorFunction[IRP_MJ_CLOSE] = DriverIoctl;
-	object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DriverIoctl;
+NTSTATUS real_main(PDRIVER_OBJECT driver_obj, PUNICODE_STRING registery_path) {
+	UNREFERENCED_PARAMETER(registery_path);
 
-	printfs("[Mdl] : �������سɹ�");
+	UNICODE_STRING dev_name, sym_link;
+	PDEVICE_OBJECT dev_obj;
+
+	RtlInitUnicodeString(&dev_name, L"\\Device\\SFeather");
+	auto status = IoCreateDevice(driver_obj, 0, &dev_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &dev_obj);
+	if (status != STATUS_SUCCESS) return status;
+
+	RtlInitUnicodeString(&sym_link, L"\\DosDevices\\SFeather");
+	status = IoCreateSymbolicLink(&sym_link, &dev_name);
+	if (status != STATUS_SUCCESS) return status;
+
+	SetFlag(dev_obj->Flags, DO_BUFFERED_IO);
+
+	for (int t = 0; t <= IRP_MJ_MAXIMUM_FUNCTION; t++)
+		driver_obj->MajorFunction[t] = unsupported_io;
+
+	driver_obj->MajorFunction[IRP_MJ_CREATE] = create_io;
+	driver_obj->MajorFunction[IRP_MJ_CLOSE] = close_io;
+	driver_obj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = ctl_io;
+	driver_obj->DriverUnload = NULL;
+
+	ClearFlag(dev_obj->Flags, DO_DEVICE_INITIALIZING);
+	return status;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT driver_obj, PUNICODE_STRING registery_path) {
+	UNREFERENCED_PARAMETER(driver_obj);
+	UNREFERENCED_PARAMETER(registery_path);
+
+	UNICODE_STRING  drv_name;
+	RtlInitUnicodeString(&drv_name, L"\\Driver\\SFeather");
+	IoCreateDriver(&drv_name, &real_main);
+	DbgPrint("LOAD\n");
 	return STATUS_SUCCESS;
 }
